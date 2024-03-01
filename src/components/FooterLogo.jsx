ea7796e15586a728bc740474c774adb6558846ce
Tolga <123948207+tolgapp@users.noreply.github.com>
2024-03-01 15:20:21 +0100

    styled the footer in mobile

--- src/components/FooterLogo.jsx
+++ src/components/FooterLogo.jsx
@@ -1,12 +1,12 @@
-import "../scss/FooterLogo.scss"
+import "../scss/FooterLogo.scss";
 
 const FooterLogo = () => {
   return (
     <section className="footerlogo">
-    <h1>CHAI.</h1>
-    <p>Tea is the elixir of life. - Lao Tzu</p>
+      <h1>CHAI.</h1>
+      <p>Tea is the elixir of life. - Lao Tzu</p>
     </section>
-  )
-}
+  );
+};
 
-export default FooterLogo
+export default FooterLogo;
