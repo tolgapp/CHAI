f43a13e284350f40f8b4e9bd45c1a971215cedaa
Tolga <123948207+tolgapp@users.noreply.github.com>
2024-03-09 16:51:13 +0100

    Updated the logo fixing with hooks

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,39 +1,26 @@
 import { Link } from "react-router-dom";
 import "../scss/Navbar.scss";
 import MobileNav from "./MobileNav";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const Navbar = () => {
   const h1Ref = useRef(null);
+  const [isSticky, setIsSticky] = useState(false);
 
-  const handleHeight = () => {
-    if (window.scrollY > 60) {
-      h1Ref.current.classList.remove("chai-nav");
-      h1Ref.current.classList.add("logo-fixed");
-    }
-  };
-
-  const removeHandle = () => {
-    if (window.scrollY < 60) {
-      h1Ref.current.classList.remove("logo-fixed");
-      h1Ref.current.classList.add("chai-nav");
-    }
+  const handleScroll = () => {
+    setIsSticky(window.scrollY > 50);
   };
 
   useEffect(() => {
-    window.addEventListener("scroll", handleHeight);
-    return () => window.removeEventListener("scroll", handleHeight);
-  });
+    window.addEventListener('scroll', handleScroll);
 
-  useEffect(() => {
-    window.addEventListener("scroll", removeHandle);
-    return () => window.removeEventListener("scroll", removeHandle);
-  });
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [])
 
   return (
     <>
       <div className="navbar">
-        <h1 className="chai-nav" ref={h1Ref}>
+        <h1 className={`chai-nav ${isSticky ? 'logo-fixed' : ''}`} ref={h1Ref}>
           <Link to={"/"}>CHAI.</Link>
         </h1>
         <div className="nav-links">
