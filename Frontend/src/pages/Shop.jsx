7d566c72941298cb0d3d9b7e8306070bb4e3be39
Tolga <123948207+tolgapp@users.noreply.github.com>
2024-03-12 15:24:53 +0100

    added loading text

--- Frontend/src/pages/Shop.jsx
+++ Frontend/src/pages/Shop.jsx
@@ -6,23 +6,30 @@ import "../scss/Coffees.scss";
 
 const Shop = () => {
   const [coffee, setCoffee] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+console.log("Out", loading)
 
   useEffect(() => {
-    
     fetch("https://fake-coffee-api.vercel.app/api")
-    .then((res) => res.json())
-    .then((data) => setCoffee(data));
-    
+      .then((res) => res.json())
+      .then((data) => setCoffee(data));
+      console.log("IN", loading);
+    setLoading(false);
   }, []);
 
   return (
     <div>
       <Navbar />
-      <section className="coffee-section">
-        {coffee.map((elt) => {
-          return <Coffees coffee={elt} key={elt._id} />;
-        })}
-      </section>
+      {loading ? (
+        <div className="loading">Loading ..</div>
+      ) : (
+        <section className="coffee-section">
+          {coffee.map((elt) => {
+            return <Coffees coffee={elt} key={elt._id} />;
+          })}
+        </section>
+      )}
       <Footer />
     </div>
   );
