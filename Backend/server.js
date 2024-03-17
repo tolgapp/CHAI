2dfa728440c55ea65f071f9e4efd6aab020a03c9
Tolga <123948207+tolgapp@users.noreply.github.com>
2024-03-17 11:37:32 +0100

    server js changes

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import coffee from "../Backend/assets/coffees.json" assert { type: "json" };
 
 const app = express();
 app.use(cors());
@@ -11,22 +10,10 @@ app.get('/', (request, response) => {
   response.send('Testing Hello World!')
 })
 
-app.get('/message', (request, response) => {
-  response.send('Testing second message')
-})
 
 app.listen(port, () => {
   console.log(`Test app listening at http://localhost:${port}`)
 })
-
-app.get("/coffee", (request, response) => {
-  response.status(200).json(coffee);
-});
-
-app.post("/coffee", (request, response) => {
-  todos.push(request.body);
-  response.status(201).json({ msg: "Coffees added" });
-});
 
 
 export default app;
